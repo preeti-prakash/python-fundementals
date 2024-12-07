02b2053d6baf1d4da36bfb0060686e2b11198881
preeti-prakash <preetiprakashkandregula@gmail.com>
2024-12-07 08:48:23 -0500

    oop - basics

---
+++ 11_oop/000_oop.py
@@ -0,0 +1,27 @@
+# STRUCTURED PROGRAMMING - code is organized around functions.
+
+def login(username, password):
+    pass
+ 
+def logout():
+    pass
+ 
+def select_product(product_id):
+    pass
+
+
+# OBJECT ORIENTED PROGRAMMING - code is organized around classes and objects.
+
+class Customer:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+    
+    def login(self):
+        pass
+ 
+    def logout(self):
+        pass
+ 
+    def select_product(self, product):
+        pass
