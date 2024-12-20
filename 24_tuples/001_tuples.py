beeda6a1cc7ab1330db6a311eafa69ec12227def
preeti-prakash <preetiprakashkandregula@gmail.com>
2024-12-20 17:13:34 -0500

    tuples and its features and methods

---
+++ 24_tuples/001_tuples.py
@@ -0,0 +1,28 @@
+# Tuples - once a tuple is created, you cannot change its content.
+
+# CREATING AND RETURNING A TUPLE
+def create_my_tuple():
+    return 'preeti', 1996, 'India'
+    
+my_tuple = create_my_tuple()
+print(my_tuple)
+print(type(my_tuple))
+
+# Output:
+# ('preeti', 1996, 'India')
+# <class 'tuple'>
+
+
+# DESTRUCTURING A TUPLE
+name, year, country = my_tuple
+print(name)       # Outputs: preeti
+print(year)       # Outputs: 1996
+print(country)    # Outputs: India
+# Note - the variables on the LHS = the values in RHS
+
+# TUPLE OPERATIONS
+
+print(len(my_tuple))  # Outputs: 3
+print(my_tuple[0])    # Outputs: preeti
+print(my_tuple[1])    # Outputs: 1996
+print(my_tuple[2])    # Outputs: India
